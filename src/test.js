7f7c66e7395ee8141dbf114b3a032bed3a0e48ac
Ilya <fadeev.ilya@gmail.com>
2015-10-23 15:57:32 -0400

    tests: added test for makeArr; added steal-qunit

--- src/test.js
+++ src/test.js
@@ -1,12 +1,20 @@
 import QUnit from 'steal-qunit';
-import VM from './view-model';
+import * as MultiSelect from './multi-select';
 
-var vm = new VM({
+var vm = new MultiSelect.VM({
   list: ['Testing 123']
 });
 
 QUnit.module('Initialize multi-select');
 
-QUnit.test('Test Description', function(assert) {
-  ok(vm.attr('amount'), 1, 'The amounts match');
+QUnit.test('makeArr', function(assert) {
+  assert.ok(true, 'The amounts match');
+  var arrayLike = {
+    0: 0,
+    1: 1,
+    length: 2
+  };
+  var array = MultiSelect.makeArr(arrayLike);
+  assert.ok(array instanceof Array, 'Should return type array');
+  assert.deepEqual(array, [0,1], 'Should have correct data');
 });
